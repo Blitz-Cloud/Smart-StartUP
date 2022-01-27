d8e3518b3a56704acb3caf2cc4fdaa2448a6c559
BLITzStudio <81566145+BLITzStudio@users.noreply.github.com>
2022-01-27 14:59:08 +0200

    Logic for reading only .ps1 files from _config

--- src/back-end/utilities.js
+++ src/back-end/utilities.js
@@ -27,7 +27,7 @@ const readFolder = async function (PATH) {
   const data = await fileSys.readdir(`${PATH}`);
   return data;
 };
-const checkProfile = function () {
+const check_Config = function () {
   return new Promise((resolve, reject) => {
     readFolder(path.join(__dirname, "../_config")).then((data) => {
       if (data.length) {
