ce5e2a6bc2976ec7efba332daf108811d92d89cf
BLITzStudio <81566145+BLITzStudio@users.noreply.github.com>
2022-01-28 12:50:24 +0200

    Update render.js

--- src/front-end/render.js
+++ src/front-end/render.js
@@ -1,9 +1,6 @@
 const { start, test, isProfile, readFolder } = window.myApi;
-isProfile()
-  .then(() => {
-    console.log("there is a profile ");
-    // this function is gonna call the createProfile() automatically on the start-up
-  })
-  .catch(() => {
-    console.log("there isn't any profile");
-  });
+isProfile().catch(() => {
+  console.log("there isn't any profile");
+  console.log("there is a profile ");
+  // this function is gonna call the createProfile() automatically on the start-up
+});
