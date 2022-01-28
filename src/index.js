e6647c7083ae6fd6d51059381ff19773edec8adb
BLITzStudio <81566145+BLITzStudio@users.noreply.github.com>
2022-01-28 18:07:27 +0200

    Reset css

--- src/index.js
+++ src/index.js
@@ -32,6 +32,7 @@ const createWindow = () => {
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
+  mainWindow.removeMenu();
 };
 
 // This method will be called when Electron has finished
