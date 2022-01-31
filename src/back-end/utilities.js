6444418bd287321e9b3aa1806f6d5ec7eec2fd34
BLITzStudio <81566145+BLITzStudio@users.noreply.github.com>
2022-01-31 19:15:59 +0200

    createProfilee() function is working as espected

--- src/back-end/utilities.js
+++ src/back-end/utilities.js
@@ -1,7 +1,6 @@
 const process = require("node:child_process");
 const path = require("node:path");
 const fileSys = require("node:fs/promises");
-
 const readFolder = async function (PATH) {
   const data = await fileSys.readdir(`${PATH}`);
   return data;
@@ -17,7 +16,6 @@ const check_Config = function () {
     });
   });
 };
-
 const checkProfile = async function () {
   return readFolder(path.join(__dirname, "../_config")).then((files) => {
     const scripts = files.filter((file) => path.extname(file) === ".ps1");
@@ -25,4 +23,21 @@ const checkProfile = async function () {
   });
 };
 
+const createProfile = async function (name, pathList) {
+  const fileName = uuidv4();
+  const routes = [];
+  pathList.forEach((path) => {
+    routes.push(`& "${path}"`);
+  });
+  console.log(routes);
+  const jsonBody = {
+    name: `${name}`,
+    path: routes,
+  };
+  fileSys.writeFile(
+    `${path.join(__dirname, "./src/_config")}/${fileName}.json`,
+    JSON.stringify(jsonBody),
+    { flag: "ax" }
+  );
+};
 module.exports = { checkProfile, readFolder };
